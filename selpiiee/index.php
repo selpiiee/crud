1c4e2de7a954895239706c9f7b4a03047e1e9e04
selpiiee <118158202+selpiiee@users.noreply.github.com>
2023-05-25 06:59:06 +0700

    Add files via upload

---
+++ selpiiee/index.php
@@ -0,0 +1,105 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CRUD Produk dengan gambar - Gilacoding</title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: salmon;
+      }
+    table {
+      border: solid 1px #DDEEEE;
+      border-collapse: collapse;
+      border-spacing: 0;
+      width: 70%;
+      margin: 10px auto 10px auto;
+    }
+    table thead th {
+        background-color: #DDEFEF;
+        border: solid 1px #DDEEEE;
+        color: #336B6B;
+        padding: 10px;
+        text-align: left;
+        text-shadow: 1px 1px 1px #fff;
+        text-decoration: none;
+    }
+    table tbody td {
+        border: solid 1px #DDEEEE;
+        color: #333;
+        padding: 10px;
+        text-shadow: 1px 1px 1px #fff;
+    }
+    a {
+          background-color: salmon;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+    }
+    </style>
+  </head>
+  <body>
+    <center><h1>Data Produk</h1><center>
+    <center><a href="tambah_produk.php">+ &nbsp; Tambah Produk</a><center>
+    <br/>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Absensi</th>
+          <th>Kelas </th>
+          <th>Gambar</th>
+          <th colspan="2">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+      $query = "SELECT * FROM produk ORDER BY id ASC";
+      $result = mysqli_query($koneksi, $query);
+      //mengecek apakah ada error ketika menjalankan query
+      if(!$result){
+        die ("Query Error: ".mysqli_errno($koneksi).
+           " - ".mysqli_error($koneksi));
+      }
+
+      //buat perulangan untuk element tabel dari data mahasiswa
+      $no = 1; //variabel untuk membuat nomor urut
+      // hasil query akan disimpan dalam variabel $data dalam bentuk array
+      // kemudian dicetak dengan perulangan while
+      while($row = mysqli_fetch_assoc($result))
+      {
+      ?>
+       <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $row['nama_produk']; ?></td>
+          <td><?php echo substr($row['deskripsi'], 0, 20); ?>...</td>
+          <td>Rp <?php echo number_format($row['harga_beli'],0,',','.'); ?></td>
+          <td>Rp <?php echo $row['harga_jual']; ?></td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;"></td>
+          <td>
+            <center><a href="asset/view/form_ubah.php?id=<?php echo $data['id'] ?>">Edit </a></center>
+          </td>
+          <td>
+            <center><a href="asset/controller/proses_hapus.php?id=<?php echo $data['id'] ?>"
+                  onclick="return confirm('Apakah anda yakin ingin menghapus data siswa <?php echo $data['nama']; ?> ? ')">
+                  Delete </a></center>
+          </td>
+      </tr>
+         
+      <?php
+        $no++; //untuk nomor urut terus bertambah 1
+      }
+      ?>
+    </tbody>
+    </table>
+  </body>
+</html>
